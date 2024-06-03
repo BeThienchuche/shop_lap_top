2736894bc30b317ece07454d49f69565ba7e0247
BeThienchuche <154670671+BeThienchuche@users.noreply.github.com>
2024-06-03 13:06:04 +0700

    Add files via upload

---
+++ Home/js/all-products.js
@@ -0,0 +1,63 @@
+//-----------ấn chuột để chuyển tiếp slider img---------
+const rightbtn = document.querySelector('.fa-chevron-right')
+const leftbtn = document.querySelector('.fa-chevron-left')
+const imgSliderNumber = document.querySelectorAll('.slider-content-left-top img')
+let index = 0
+
+rightbtn.addEventListener ("click",function(){
+  index++  //vòng lặp next ảnh khi ấn chuột
+  if(index>imgSliderNumber.length-1){
+    index=0
+  }
+  document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+})
+leftbtn.addEventListener ("click",function(){
+  index--   //vòng lặp lùi ảnh khi ấn chuột
+  if(index<=0){
+    index=imgSliderNumber.length-1
+  }
+  document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+})
+//-------xử lý slider content bottom--------
+const imgNumberLi = document.querySelectorAll('.slider-content-left-bottom li')
+imgNumberLi.forEach(function(image,index){
+    image.addEventListener("click",function(){
+    removeactive() 
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%" //nội dung theo ảnh
+    image.classList.add("active")
+    })
+})
+  function removeactive(){
+    let imgactive = document.querySelector('.active')
+    imgactive.classList.remove("active")
+  }
+  function removeactive(){
+    let imgactive = document.querySelectorAll('.slider-content-left-bottom li.active') 
+    imgactive.forEach(function(img) {
+      img.classList.remove("active")
+    })
+  }
+
+  //---------------slider img chạy tự động-----------
+  function imgAuto(){
+    index++
+    if(index>imgSliderNumber.length-1){
+      index = 0
+    }
+    removeactive() 
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+    imgNumberLi[index].classList.add("active")
+  }
+  setInterval(imgAuto,4500) //thời gian chuyển tiếp ảnh 4.5s
+
+  //------------xử lý nút xem thêm để hiện thêm sản phẩm và ẩn nút xem thêm khi ấn
+const showButton = document.getElementById('show-all');
+const hiddenProducts = document.querySelectorAll('.hidden');
+
+showButton.addEventListener('click', () => {
+  hiddenProducts.forEach((product) => {
+    product.classList.remove('hidden');
+  });
+  showButton.style.display = 'none';
+});
+ 
